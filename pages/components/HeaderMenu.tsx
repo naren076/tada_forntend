b6749d50d5e075e3481d705da4bb4ab6931cf6d9
Narayanan Ganesan <narayanan.g@mksvision.com>
2023-09-21 11:16:54 +0530

    Modified CSS

--- pages/components/HeaderMenu.tsx
+++ pages/components/HeaderMenu.tsx
@@ -1,21 +1,57 @@
-import React from 'react';
-import { Layout, Menu, theme } from 'antd';
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
+import type { MenuProps } from "antd";
 
 const { Header } = Layout;
 
+const items: MenuProps["items"] = [
+  {
+    label: "Home",
+    key: "1",
+  },
+];
+
+const itemsright: MenuProps["items"] = [
+  {
+    label: "Experiment",
+    key: "2",
+  },
+  {
+    label: "Help",
+    key: "3",
+  },
+  {
+    label: "Login",
+    key: "4",
+  },
+];
+
+
 export const HeaderMenu: React.FC = () => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+  const [current, setCurrent] = useState("1");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
 
   return (
-    <Layout className="layout">
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <div className="demo-logo" />
-        <ul>
-          Home
-        </ul>
-      </Header>
-    </Layout>
+    <Header style={{ display: "flex", alignItems: "center" }}>
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={items}
+          disabledOverflow={true}
+        />
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={itemsright}
+          disabledOverflow={true}
+          className="ml-auto"
+        />
+    </Header>
   );
 };
